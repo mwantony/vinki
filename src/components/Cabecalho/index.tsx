c6c1ad06fe981c8b980d5f749d8cf5ac1569f6d6
Antony Maziero Warken <againplayi7@gmail.com>
2023-05-27 17:43:11 -0300

    commit

--- src/components/Cabecalho/index.tsx
+++ src/components/Cabecalho/index.tsx
@@ -4,6 +4,7 @@ import {ReactComponent as MenuItem} from '../../assets/svg/menu-item.svg'
 import {ReactComponent as Vinki} from '../../assets/svg/vinki.svg'
 import {ReactComponent as Profile} from '../../assets/svg/profile.svg'
 import {ReactComponent as Cart} from '../../assets/svg/cart.svg'
+import {ReactComponent as SearchIcon} from '../../assets/svg/search-icon.svg'
 export default function Cabecalho() {
     return(
         <header className={styles.cabecalho}>
@@ -12,6 +13,7 @@ export default function Cabecalho() {
                 <Vinki className={styles['navegacao__logo']}></Vinki>
                 <input type="search" name="" id="" className={styles['navegacao__search']} placeholder='Busque aqui'/>
                 <div className={styles['navegacao__account']}>
+                    <SearchIcon className={styles['navegacao__searchicon']}></SearchIcon>
                     <Profile className={styles['navegacao__profile']}></Profile>
                     <Cart className={styles['navegacao__cart']}></Cart>
                 </div>
--- src/pages/Inicio/index.tsx
+++ src/pages/Inicio/index.tsx
@@ -4,19 +4,127 @@ import moveis from "../../assets/img/moveis.png";
 import eletronicos from "../../assets/img/eletronicos.png";
 import decoracoes from "../../assets/img/decoracoes.png";
 import jardim from "../../assets/img/jardim.png";
-import produtos from '../../db.json';
+import produtos from "../../db.json";
+import React from "react";
+import Slider from "react-slick";
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
+import { ReactComponent as ArrowBackIosIcon } from "../../assets/svg/prevbutton.svg";
+import { ReactComponent as ArrowNextIosIcon } from "../../assets/svg/nextbutton.svg";
 export default function Inicio() {
   const carousel = [
     {
-      link: "https://cdn.discordapp.com/attachments/799817839484731397/1111705400463593482/f1.png",
+      link: "https://cdn.discordapp.com/attachments/799817839484731397/1112064051233689650/f1-mobile.png",
     },
     {
-      link: "https://cdn.discordapp.com/attachments/799817839484731397/1111705400463593482/f1.png",
+      link: "https://cdn.discordapp.com/attachments/799817839484731397/1112064051233689650/f1-mobile.png",
     },
     {
-      link: "https://cdn.discordapp.com/attachments/799817839484731397/1111705400463593482/f1.png",
+      link: "https://cdn.discordapp.com/attachments/799817839484731397/1112064051233689650/f1-mobile.png",
     },
   ];
+  const handleDragStart = (e: any) => e.preventDefault();
+  const produtosEmDestaque = [
+    <li className={styles["emdestaque__lista--item"]}>
+      <div
+        className={styles["lista__imagem"]}
+        style={{ backgroundImage: "url('../../assets/img/emdestaque1.png')" }}
+      ></div>
+      <caption className="lista__legenda">
+        <h3 className={styles["lista__subtitulo"]}>Sofá retrátil</h3>
+        <p className={styles["lista__categoria"]}>Móveis</p>
+        <p className={styles["lista__preco"]}>R$ 2500,00</p>
+        <h3 className={styles["lista__promo"]}>R$ 1059,90</h3>
+        <button className={styles["lista__comprar"]}>Comprar</button>
+      </caption>
+    </li>,
+    <li className={styles["emdestaque__lista--item"]}>
+      <div
+        className={styles["lista__imagem"]}
+        style={{ backgroundImage: "url('../../assets/img/emdestaque1.png')" }}
+      ></div>
+      <caption className="lista__legenda">
+        <h3 className={styles["lista__subtitulo"]}>Sofá retrátil</h3>
+        <p className={styles["lista__categoria"]}>Móveis</p>
+        <p className={styles["lista__preco"]}>R$ 2500,00</p>
+        <h3 className={styles["lista__promo"]}>R$ 1059,90</h3>
+        <button className={styles["lista__comprar"]}>Comprar</button>
+      </caption>
+    </li>,
+    <li className={styles["emdestaque__lista--item"]}>
+      <div
+        className={styles["lista__imagem"]}
+        style={{ backgroundImage: "url('../../assets/img/emdestaque1.png')" }}
+      ></div>
+      <caption className="lista__legenda">
+        <h3 className={styles["lista__subtitulo"]}>Sofá retrátil</h3>
+        <p className={styles["lista__categoria"]}>Móveis</p>
+        <p className={styles["lista__preco"]}>R$ 2500,00</p>
+        <h3 className={styles["lista__promo"]}>R$ 1059,90</h3>
+        <button className={styles["lista__comprar"]}>Comprar</button>
+      </caption>
+    </li>,
+    <li className={styles["emdestaque__lista--item"]}>
+      <div
+        className={styles["lista__imagem"]}
+        style={{ backgroundImage: "url('../../assets/img/emdestaque1.png')" }}
+      ></div>
+      <caption className="lista__legenda">
+        <h3 className={styles["lista__subtitulo"]}>Sofá retrátil</h3>
+        <p className={styles["lista__categoria"]}>Móveis</p>
+        <p className={styles["lista__preco"]}>R$ 2500,00</p>
+        <h3 className={styles["lista__promo"]}>R$ 1059,90</h3>
+        <button className={styles["lista__comprar"]}>Comprar</button>
+      </caption>
+    </li>,
+  ];
+  const items = [
+    <img
+      src={moveis}
+      onDragStart={handleDragStart}
+      role="presentation"
+      alt="Foto Móvel"
+      className={styles["lista__foto--slicer"]}
+    />,
+    <img
+      src={eletronicos}
+      onDragStart={handleDragStart}
+      role="presentation"
+      alt="Foto Eletrônico"
+      className={styles["lista__foto--slicer"]}
+    />,
+    <img
+      src={decoracoes}
+      onDragStart={handleDragStart}
+      role="presentation"
+      alt="Foto Decoração"
+      className={styles["lista__foto--slicer"]}
+    />,
+    <img
+      src={jardim}
+      onDragStart={handleDragStart}
+      role="presentation"
+      alt="Foto Jardim"
+      className={styles["lista__foto--slicer"]}
+    />,
+  ];
+  const responsive = {
+    2000: {
+      items: 11,
+    },
+    1200: {
+      items: 5,
+    },
+    800: {
+      items: 3,
+    },
+    500: {
+      items: 2,
+    },
+    0: {
+      items: 1,
+    },
+  };
   const categorias = [
     {
       link: moveis,
@@ -35,6 +143,16 @@ export default function Inicio() {
       label: "Foto do jardim",
     },
   ];
+  const renderPrevButton = () => {
+    return (
+      <ArrowBackIosIcon style={{ position: "absolute", left: 0, top: 130 }} />
+    );
+  };
+  const renderNextButton = () => {
+    return (
+      <ArrowNextIosIcon style={{ position: "absolute", right: 0, top: 130 }} />
+    );
+  };
   return (
     <>
       <section>
@@ -98,36 +216,79 @@ export default function Inicio() {
             <span className="visually-hidden">Next</span>
           </button>
         </div>
+
         <ArrowDown className={styles.arrowdown}></ArrowDown>
-        <ul className={styles["lista--categoria"]}>
-          {categorias.map((item, index) => {
+        <AliceCarousel
+          mouseTracking
+          items={items}
+          disableDotsControls={true}
+          responsive={responsive}
+          renderPrevButton={renderPrevButton}
+          renderNextButton={renderNextButton}
+        />
+      </section>
+      <section className={styles["emdestaque"]}>
+        <h2 className={styles["emdestaque__titulo"]}>Produtos em destaque</h2>
+        <div id="slider" className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth">
+        <li className={styles["emdestaque__lista--item"]}>
+                <div
+                  className={styles["lista__imagem"]}
+                  style={{ backgroundImage: "url('../../assets/img/emdestaque1.png')" }}
+                ></div>
+                <div className={styles["lista__legenda"]}>
+                  <h3 className={styles["lista__subtitulo"]}>Sofá retrátil</h3>
+                  <p className={styles["lista__categoria"]}>Móveis</p>
+                  <p className={styles["lista__preco"]}>R$ 2500,00</p>
+                  <h3 className={styles["lista__promo"]}>R$ 1059,90</h3>
+                  <button className={styles["lista__comprar"]}>Comprar</button>
+                </div>
+              </li>
+              <li className={styles["emdestaque__lista--item"]}>
+                <div
+                  className={styles["lista__imagem"]}
+                  style={{ backgroundImage: "url('../../assets/img/emdestaque1.png')" }}
+                ></div>
+                <div className={styles["lista__legenda"]}>
+                  <h3 className={styles["lista__subtitulo"]}>Sofá retrátil</h3>
+                  <p className={styles["lista__categoria"]}>Móveis</p>
+                  <p className={styles["lista__preco"]}>R$ 2500,00</p>
+                  <h3 className={styles["lista__promo"]}>R$ 1059,90</h3>
+                  <button className={styles["lista__comprar"]}>Comprar</button>
+                </div>
+              </li>
+              <li className={styles["emdestaque__lista--item"]}>
+                <div
+                  className={styles["lista__imagem"]}
+                  style={{ backgroundImage: "url('../../assets/img/emdestaque1.png')" }}
+                ></div>
+                <div className={styles["lista__legenda"]}>
+                  <h3 className={styles["lista__subtitulo"]}>Sofá retrátil</h3>
+                  <p className={styles["lista__categoria"]}>Móveis</p>
+                  <p className={styles["lista__preco"]}>R$ 2500,00</p>
+                  <h3 className={styles["lista__promo"]}>R$ 1059,90</h3>
+                  <button className={styles["lista__comprar"]}>Comprar</button>
+                </div>
+              </li>
+        </div>
+        <ul className={styles["emdestaque__lista"]}>
+          {produtos.map((item, index) => {
             return (
-              <li className={styles["lista--categoria__item"]}>
-                <img src={item.link} alt={item.label} />
+              <li className={styles["emdestaque__lista--item"]}>
+                <div
+                  className={styles["lista__imagem"]}
+                  style={{ backgroundImage: "url(" + item.foto + ")" }}
+                ></div>
+                <div className={styles["lista__legenda"]}>
+                  <h3 className={styles["lista__subtitulo"]}>Sofá retrátil</h3>
+                  <p className={styles["lista__categoria"]}>Móveis</p>
+                  <p className={styles["lista__preco"]}>R$ 2500,00</p>
+                  <h3 className={styles["lista__promo"]}>R$ 1059,90</h3>
+                  <button className={styles["lista__comprar"]}>Comprar</button>
+                </div>
               </li>
             );
           })}
         </ul>
-      </section>
-      <section className={styles['emdestaque']}>
-        <h2 className={styles['emdestaque__titulo']}>Produtos em destaque</h2>
-        <ul className={styles['emdestaque__lista']}>
-          {produtos.map((item, index) => {
-            return(
-              <li className={styles['emdestaque__lista--item']}>
-                  <div className={styles['lista__imagem']} style={{backgroundImage: 'url(' + item.foto + ')'}} ></div>
-                  <caption className="lista__legenda">
-                    <h3 className={styles['lista__subtitulo']}>Sofá retrátil</h3>
-                    <p className={styles['lista__categoria']}>Móveis</p>
-                    <p className={styles['lista__preco']}>R$ 2500,00</p>
-                    <h3 className={styles['lista__promo']}>R$ 1059,90</h3>
-                    <button className={styles['lista__comprar']}>Comprar</button>
-                  </caption>
-              </li>
-            )
-          })}
-        </ul>
-
       </section>
     </>
   );
