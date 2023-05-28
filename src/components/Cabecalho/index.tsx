eb70a80e72d7a939e8d7d7a16a13a6cc890870e0
Antony Maziero Warken <againplayi7@gmail.com>
2023-05-28 14:35:02 -0300

    asdasd

--- src/components/Cabecalho/index.tsx
+++ src/components/Cabecalho/index.tsx
@@ -1,23 +1,102 @@
-import styles from './Cabecalho.module.scss'
+import styles from "./Cabecalho.module.scss";
+import classNames from "classnames";
 
-import {ReactComponent as MenuItem} from '../../assets/svg/menu-item.svg'
-import {ReactComponent as Vinki} from '../../assets/svg/vinki.svg'
-import {ReactComponent as Profile} from '../../assets/svg/profile.svg'
-import {ReactComponent as Cart} from '../../assets/svg/cart.svg'
-import {ReactComponent as SearchIcon} from '../../assets/svg/search-icon.svg'
-export default function Cabecalho() {
-    return(
-        <header className={styles.cabecalho}>
-            <nav className={styles.navegacao}>
-                <MenuItem className={styles['navegacao__menu']}></MenuItem>
-                <Vinki className={styles['navegacao__logo']}></Vinki>
-                <input type="search" name="" id="" className={styles['navegacao__search']} placeholder='Busque aqui'/>
-                <div className={styles['navegacao__account']}>
-                    <SearchIcon className={styles['navegacao__searchicon']}></SearchIcon>
-                    <Profile className={styles['navegacao__profile']}></Profile>
-                    <Cart className={styles['navegacao__cart']}></Cart>
-                </div>
-            </nav>
-        </header>
-    )
+import { ReactComponent as MenuItem } from "../../assets/svg/menu-item.svg";
+import { ReactComponent as Vinki } from "../../assets/svg/vinki.svg";
+import { ReactComponent as Profile } from "../../assets/svg/profile.svg";
+import { ReactComponent as Cart } from "../../assets/svg/cart.svg";
+import { ReactComponent as SearchIcon } from "../../assets/svg/search-icon.svg";
+import { Link } from "react-router-dom";
+interface Props {
+  aparecer: boolean;
+  setAparecer: React.Dispatch<React.SetStateAction<boolean>>;
+  selecionado: number;
+  setSelecionado: React.Dispatch<React.SetStateAction<number>>;
+}
+export default function Cabecalho({
+  aparecer,
+  setAparecer,
+  selecionado,
+  setSelecionado,
+}: Props) {
+  const menu = [
+    {
+      titulo: "Início",
+      to: "/",
+      classe: "inicio",
+    },
+    {
+      titulo: "Carrinho",
+      to: "carrinho",
+      classe: "carrinho",
+    },
+    {
+      titulo: "Favoritos",
+      to: "favoritos",
+      classe: "favoritos",
+    },
+    {
+      titulo: "Em alta",
+      to: "emalta",
+      classe: "emalta",
+    },
+    {
+      titulo: "Ofertas",
+      to: "ofertas",
+      classe: "ofertas",
+    },
+  ];
+  return (
+    <header className={styles.cabecalho}>
+      <nav className={styles.navegacao}>
+        <MenuItem className={styles["navegacao__menu"]} onClick={() => {
+            console.log('sd')
+        setAparecer(true)
+        }}></MenuItem>
+        <Vinki className={styles["navegacao__logo"]}></Vinki>
+        <input
+          type="search"
+          name=""
+          id=""
+          className={styles["navegacao__search"]}
+          placeholder="Busque aqui"
+        />
+        <div className={styles["navegacao__account"]}>
+          <SearchIcon className={styles["navegacao__searchicon"]}></SearchIcon>
+          <Profile className={styles["navegacao__profile"]}></Profile>
+          <Cart className={styles["navegacao__cart"]}></Cart>
+        </div>
+      </nav>
+      <div className={styles["lista__div"]}></div>
+      <div onClick={() => setAparecer(false)} className={classNames({
+          [styles.divisao__menu]: true,
+          [styles["divisao__menu--aparecer"]]: aparecer === true ? true : false,
+        })}></div>
+      <ul
+        className={classNames({
+          [styles.menu]: true,
+          [styles["menu--aparecer"]]: aparecer === true ? true : false,
+        })}
+      >
+        <div className={styles["menu__links"]}>
+          {menu.map((item, index) => {
+            return (
+              <Link to={item.to} onClick={() => setSelecionado(index)}>
+                <li
+                  className={classNames({
+                    [styles["menu__item"]]: true,
+                    [styles[`menu__item--${item.classe}`]]: true,
+                    [styles["menu__item--selecionado"]]:
+                      selecionado === index ? true : false,
+                  })}
+                >
+                  &nbsp;&nbsp;&nbsp;{item.titulo}
+                </li>
+              </Link>
+            );
+          })}
+        </div>
+      </ul>
+    </header>
+  );
 }
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import React from "react";
+import React, { useState } from "react";
 import Inicio from "./pages/Inicio";
 import Cabecalho from "./components/Cabecalho";
 import Rodape from "./components/Rodape";
 export default function AppRouter() {
+    const [aparecer, setAparecer] = useState(false)
+    const [selecionado, setSelecionado] = useState(0)
     return(
         <BrowserRouter>
-            <Cabecalho></Cabecalho>
+            <Cabecalho selecionado={selecionado} setSelecionado={setSelecionado} aparecer={aparecer} setAparecer={setAparecer}></Cabecalho>
             <Routes>
                 <Route path="/" element={<Inicio></Inicio>}></Route>
             </Routes>
